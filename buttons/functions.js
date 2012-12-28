a7211516e34b23be9c20d11443dec77b29ce1710
Marcelismus <Marcelismus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-28 17:04:16 +0000

    Version 1.1.2
    git-svn-id: https://plugins.svn.wordpress.org/wp-appbox/tags/1.1.2@645424 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ buttons/functions.js
@@ -0,0 +1,23 @@
+function wpAppbox_appstore_button() {
+	return "[appbox appstore ]";
+}
+
+function wpAppbox_googleplay_button() {
+	return "[appbox googleplay ]";
+}
+
+function wpAppbox_androidpit_button() {
+	return "[appbox androidpit ]";
+}
+
+function wpAppbox_windowsphonestore_button() {
+	return "[appbox windowsphone ]";
+}
+
+function wpAppbox_windowsstore_button() {
+	return "[appbox windowsstore ]";
+}
+
+function wpAppbox_bbappworld_button() {
+	return "[appbox appworld ]";
+}
---
+++ inc/definitions.php
@@ -0,0 +1,18 @@
+<?php 
+$options = get_option('wpAppbox');
+if($options['wpAppbox_piccachetime'] == '') $options['wpAppbox_piccachetime'] = '180';
+if($options['wpAppbox_datacachetime'] == '') $options['wpAppbox_datacachetime'] = '180';
+
+define("WPAPPBOX_PLUGIN_NAME", 'WP-Appbox'); 
+define("WPAPPBOX_PLUGIN_VERSION", '1.1.2');
+
+define("WPAPPBOX_PIC_CACHINGTIME", $options['wpAppbox_piccachetime']); 
+define("WPAPPBOX_DATA_CACHINGTIME", $options['wpAppbox_datacachetime']);  
+
+define("WPAPPBOX_ERRROR_PARSEOUTPUT", $options['wpAppbox_error_parseoutput']); 
+
+define("PLUGIN_BASE_DIR", basename(dirname(dirname(__FILE__))));
+
+define("WPAPPBOX_CONTENT_URL", WP_PLUGIN_URL."/".PLUGIN_BASE_DIR."/cache/"); // URL to Cache Folder (www.site.com/...)
+define("WPAPPBOX_CONTENT_DIR", WP_PLUGIN_DIR."/".PLUGIN_BASE_DIR."/cache/"); // lokal Path to cache Folder (var/www/htdocs/...)
+?>
