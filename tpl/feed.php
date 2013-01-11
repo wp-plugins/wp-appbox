2edb3449e18ff0d80a5d134437745407a129a92a
Marcelismus <Marcelismus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-11 18:06:58 +0000

    Version 1.2.2
    git-svn-id: https://plugins.svn.wordpress.org/wp-appbox/tags/1.2.2@651404 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tpl/feed.php
@@ -0,0 +1,11 @@
+<div class="appcontainer feed <?php storename_css(); ?>">
+	<table width="100%;">
+		<tr>
+			<td style="width: 88px;"><a href="<?php app_store_url(); ?>" title="<?php app_title(); ?>"><img style="border-radius: 6px !important; border: 0 !important; padding: 0 !important; width: 80px !important; height: 80px !important;" src="<?php banner_icon(); ?>" alt="<?php app_title(); ?>" /></a></td>
+			<td>
+				<a href="<?php app_store_url(); ?>" title="<?php app_title(); ?>"><?php app_title(); ?><img src="<?php echo plugins_url('img/'.get_storename_css().'-small.png', dirname(__FILE__)); ?>" style="margin-left:6px !important;" /></a><br />
+				Preis: <?php app_price(); ?>
+			</td>
+		</tr>
+	</table>
+</div>
