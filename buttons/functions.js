2f4b897a3abf37eaccfec6d7d424426deee8fd3a
Marcelismus <Marcelismus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-18 07:15:52 +0000

    Version 1.5.2
    git-svn-id: https://plugins.svn.wordpress.org/wp-appbox/tags/1.5.2@714779 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ buttons/functions.js
@@ -0,0 +1,39 @@
+function wpAppbox_appstore_button() {
+	return "[appbox appstore ]";
+}
+
+function wpAppbox_googleplay_button() {
+	return "[appbox googleplay ]";
+}
+
+function wpAppbox_androidpit_button() {
+	return "[appbox androidpit ]";
+}
+
+function wpAppbox_windowsphonestore_button() {
+	return "[appbox windowsphone ]";
+}
+
+function wpAppbox_windowsstore_button() {
+	return "[appbox windowsstore ]";
+}
+
+function wpAppbox_blackberryworld_button() {
+	return "[appbox blackberryworld ]";
+}
+
+function wpAppbox_firefoxmarketplace_button() {
+	return "[appbox firefoxmarketplace ]";
+}
+
+function wpAppbox_chromewebstore_button() {
+	return "[appbox chromewebstore ]";
+}
+
+function wpAppbox_firefoxaddon_button() {
+	return "[appbox firefoxaddon ]";
+}
+
+function wpAppbox_samsungapps_button() {
+	return "[appbox samsungapps ]";
+}
---
+++ buttons/single.btn.js.php
@@ -0,0 +1,122 @@
+<?php include(dirname(__FILE__).'/functions.js'); ?>
+
+(function() {
+	tinymce.create('tinymce.plugins.wpAppbox_StoreButtons', {
+
+		init : function(ed, url){
+			ed.addButton('wpAppbox_AppStoreButton', {
+			title : '(Mac) App Store Appbox',
+				onclick : function() {
+					ed.execCommand(
+						'mceInsertContent',
+						false,
+						wpAppbox_appstore_button()
+					);
+				},
+				image: url + "/appstore.btn.png"
+			});
+			
+			ed.addButton('wpAppbox_GooglePlayButton', {
+			title : '(Mac) App Store Appbox',
+				onclick : function() {
+					ed.execCommand(
+						'mceInsertContent',
+						false,
+						wpAppbox_googleplay_button()
+					);
+				},
+				image: url + "/googleplay.btn.png"
+			});
+			
+			ed.addButton('wpAppbox_AndroidPitButton', {
+			title : '(Mac) App Store Appbox',
+				onclick : function() {
+					ed.execCommand(
+						'mceInsertContent',
+						false,
+						wpAppbox_androidpit_button()
+					);
+				},
+				image: url + "/androidpit.btn.png"
+			});
+		
+			ed.addButton('wpAppbox_WindowsPhoneStoreButton', {
+			title : 'Windows Phone Store Appbox',
+				onclick : function() {
+					ed.execCommand(
+						'mceInsertContent',
+						false,
+						wpAppbox_windowsphonestore_button()
+					);
+				},
+				image: url + "/windowsphonestore.btn.png"
+			});
+		
+		
+			ed.addButton('wpAppbox_FirefoxMarketplaceButton', {
+			title : 'Firefox Marketplace Appbox',
+				onclick : function() {
+					ed.execCommand(
+						'mceInsertContent',
+						false,
+						wpAppbox_firefoxmarketplace_button()
+					);
+				},
+				image: url + "/firefoxmarketplace.btn.png"
+			});
+		
+		
+			ed.addButton('wpAppbox_ChromeWebStoreButton', {
+			title : 'Chrome Web Store Appbox',
+				onclick : function() {
+					ed.execCommand(
+						'mceInsertContent',
+						false,
+						wpAppbox_chromewebstore_button()
+					);
+				},
+				image: url + "/chromewebstore.btn.png"
+			});
+		
+			ed.addButton('wpAppbox_WindowsStoreButton', {
+			title : 'Windows Store Appbox',
+				onclick : function() {
+					ed.execCommand(
+						'mceInsertContent',
+						false,
+						wpAppbox_windowsstore_button()
+					);
+				},
+				image: url + "/windowsstore.btn.png"
+			});
+		
+			ed.addButton('wpAppbox_FirefoxAddonButton', {
+			title : 'Firefox Erweiterungen',
+				onclick : function() {
+					ed.execCommand(
+						'mceInsertContent',
+						false,
+						wpAppbox_firefoxaddon_button()
+					);
+				},
+				image: url + "/firefoxaddon.btn.png"
+			});
+		
+			ed.addButton('wpAppbox_SamsungAppsButton', {
+			title : 'Samsung Apps',
+				onclick : function() {
+					ed.execCommand(
+						'mceInsertContent',
+						false,
+						wpAppbox_samsungapps_button()
+					);
+				},
+				image: url + "/samsungapps.btn.png"
+			});
+			
+		}
+		
+	});
+
+	tinymce.PluginManager.add('wpAppbox', tinymce.plugins.wpAppbox_StoreButtons);
+})();
