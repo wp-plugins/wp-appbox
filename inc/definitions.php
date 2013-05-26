2717192d1caa06aa5066f6115e571e5793a1bb34
Marcelismus <Marcelismus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-26 10:04:49 +0000

    Version 1.6.0
    git-svn-id: https://plugins.svn.wordpress.org/wp-appbox/tags/1.6.0@718434 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/definitions.php
@@ -0,0 +1,32 @@
+<?php 
+$options = get_option('wpAppbox');
+if($options['wpAppbox_datacachetime'] == '') $options['wpAppbox_datacachetime'] = '300';
+
+define("WPAPPBOX_PLUGIN_NAME", 'WP-Appbox'); 
+define("WPAPPBOX_PLUGIN_VERSION", '1.6.0');
+
+define("WPAPPBOX_DATA_CACHINGTIME", $options['wpAppbox_datacachetime']);  
+
+define("WPAPPBOX_ERRROR_PARSEOUTPUT", $options['wpAppbox_error_parseoutput']); 
+
+define("PLUGIN_BASE_DIR", basename(dirname(dirname(__FILE__))));
+
+define("WPAPPBOX_CACHE_PREFIX", 'wpAppbox_');
+
+$store_names = array(	'appstore' => '(Mac) App Store',
+						'googleplay' => 'Google Play',
+						'windowsstore' => 'Windows Store',
+						'windowsphone' => 'Windows Phone Store',
+						//'blackberryworld' => 'BlackBerry World',
+						'androidpit' => 'AndroidPit',
+						'chromewebstore' => 'Chrome Web Store',
+						'firefoxmarketplace' => 'Firefox Marketplace',
+						'firefoxaddon' => 'Firefox Add-ons',
+						'amazonapps' => 'Amazon App Shop',
+						'samsungapps' => 'Samsung Apps');
+												
+$style_names = array(	'0' => 'simple',
+						'1' => 'banner',
+						'2' => 'screenshots');		
+						
+?>
