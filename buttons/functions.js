113c552ea0b9447ed19f29be66e4d8427689de13
Marcelismus <Marcelismus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-30 11:50:57 +0000

    Version 1.7.0
    git-svn-id: https://plugins.svn.wordpress.org/wp-appbox/tags/1.7.0@720355 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ buttons/functions.js
@@ -0,0 +1,47 @@
+function wpAppbox_appstore_button() {
+	return "[appbox appstore "+tinyMCE.activeEditor.selection.getContent()+"]";
+}
+
+function wpAppbox_googleplay_button() {
+	return "[appbox googleplay "+tinyMCE.activeEditor.selection.getContent()+"]";
+}
+
+function wpAppbox_androidpit_button() {
+	return "[appbox androidpit "+tinyMCE.activeEditor.selection.getContent()+"]";
+}
+
+function wpAppbox_windowsphonestore_button() {
+	return "[appbox windowsphone "+tinyMCE.activeEditor.selection.getContent()+"]";
+}
+
+function wpAppbox_windowsstore_button() {
+	return "[appbox windowsstore "+tinyMCE.activeEditor.selection.getContent()+"]";
+}
+
+function wpAppbox_blackberryworld_button() {
+	return "[appbox blackberryworld "+tinyMCE.activeEditor.selection.getContent()+"]";
+}
+
+function wpAppbox_firefoxmarketplace_button() {
+	return "[appbox firefoxmarketplace "+tinyMCE.activeEditor.selection.getContent()+"]";
+}
+
+function wpAppbox_firefoxaddon_button() {
+	return "[appbox firefoxaddon "+tinyMCE.activeEditor.selection.getContent()+"]";
+}
+
+function wpAppbox_samsungapps_button() {
+	return "[appbox samsungapps "+tinyMCE.activeEditor.selection.getContent()+"]";
+}
+
+function wpAppbox_wordpress_button() {
+	return "[appbox wordpress "+tinyMCE.activeEditor.selection.getContent()+"]";
+}
+
+function wpAppbox_chromewebstore_button() {
+	return "[appbox chromewebstore "+tinyMCE.activeEditor.selection.getContent()+"]";
+}
+
+function wpAppbox_amazonapps_button() {
+	return "[appbox amazonapps "+tinyMCE.activeEditor.selection.getContent()+"]";
+}
