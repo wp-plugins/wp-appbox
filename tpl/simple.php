3627bed70cbfc4b6406af647dabdee2ed3f0de15
Marcelismus <Marcelismus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-19 17:44:48 +0000

    Version 2.0.2
    git-svn-id: https://plugins.svn.wordpress.org/wp-appbox/tags/2.0.2@758627 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tpl/simple.php
@@ -0,0 +1,10 @@
+<div class="wpappbox simple {STORECSS}">
+	<a href="{APPLINK}" title="{TITLE}"><img class="appicon" src="{ICON}" alt="{TITLE}" /></a>
+	{QRCODE}
+	<a href="{APPLINK}"  title="{TITLE}" class="appbutton {STORECSS}"><span><?php _e('Download', 'wp-appbox'); ?> @<br />{STORE}</span></a>
+	<div class="appdetails">
+		<div class="apptitle">{RELOADLINK}<a href="{APPLINK}" title="{TITLE}" class="apptitle">{TITLE}</a></div>
+		<div class="developer"><?php _e('Entwickler', 'wp-appbox'); ?>: {DEVELOPERLINK}</div>
+		<div class="price"><?php _e('Preis', 'wp-appbox'); ?>: {PRICE}</div>
+	</div>
+</div>
