8b465a727feadc405c6c97bbaa118e7c21b9017b
Marcelismus <Marcelismus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-16 10:14:18 +0000

    Version 3.1.0
    git-svn-id: https://plugins.svn.wordpress.org/wp-appbox/tags/3.1.0@1199974 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/admin-help.php
@@ -0,0 +1,29 @@
+<h3><?php _e('Get the ID of an app', 'wp-appbox'); ?></h3>
+<p><?php _e('A small overview of how you get the ID of an app.', 'wp-appbox'); ?></p>
+
+<table class="form-table">
+	<?php foreach($store_names as $id => &$name) { ?>
+	<tr valign="top">
+		<th scope="row"><?php echo $name; ?>:</th>
+		<td><img src="<?php echo plugins_url('img/appid/'.$id.'.jpg', dirname(__FILE__)); ?>" alt="<?php echo $name; ?> ID" /></td>
+	</tr>
+	<?php } ?>
+</table>
+
+<h3><?php _e('The shortcode', 'wp-appbox'); ?></h3>
+<p><?php _e('A short overview of tags and parameters.', 'wp-appbox'); ?></p>
+
+<table class="form-table">
+	<tr valign="top">
+		<th scope="row"><?php _e('App Store screenshots', 'wp-appbox'); ?>:</th>
+		<td><?php _e('Link to a universal app for iOS and Watch-App for Apple Watch, so you can just leave on request show the iPhone or iPad screenshots. For this, <code>-iphone</code>, <code>-ipad</code> or <code>-watch</code> can be added to the App ID. Example: <code>[appbox appstore appid-iphone screenshots]</code>', 'wp-appbox'); ?></td>
+	</tr>
+	<tr valign="top">
+		<th scope="row"><?php _e('Show old price', 'wp-appbox'); ?>:</th>
+		<td><?php _e('If you want besides a special price also specify an old price, then the day <code>oldprice="xy"</code> are used in the shortcode. This is only displayed if it differs from the current price. Example: <code>[appbox appstore appid oldprice="1,99€"]</code>', 'wp-appbox'); ?></td>
+	</tr>
+	<tr valign="top">
+		<th scope="row"><?php _e('Temporarily disable cache', 'wp-appbox'); ?>:</th>
+		<td><?php _e('The cache can be temporarily disabled by adding <code>?wpappbox_reload_cache</code> is appended to the URL of an article. Only for authors.', 'wp-appbox'); ?></td>
+	</tr>
+</table>
---
+++ buttons/functions.js
@@ -0,0 +1,51 @@
+function wpAppbox_amazonapps_button() {
+	return "[appbox amazonapps "+tinyMCE.activeEditor.selection.getContent()+"]";
+}
+
+function wpAppbox_appstore_button() {
+	return "[appbox appstore "+tinyMCE.activeEditor.selection.getContent()+"]";
+}
+
+function wpAppbox_chromewebstore_button() {
+	return "[appbox chromewebstore "+tinyMCE.activeEditor.selection.getContent()+"]";
+}
+
+function wpAppbox_firefoxaddon_button() {
+	return "[appbox firefoxaddon "+tinyMCE.activeEditor.selection.getContent()+"]";
+}
+
+function wpAppbox_firefoxmarketplace_button() {
+	return "[appbox firefoxmarketplace "+tinyMCE.activeEditor.selection.getContent()+"]";
+}
+
+function wpAppbox_goodoldgames_button() {
+	return "[appbox goodoldgames "+tinyMCE.activeEditor.selection.getContent()+"]";
+}
+
+function wpAppbox_googleplay_button() {
+	return "[appbox googleplay "+tinyMCE.activeEditor.selection.getContent()+"]";
+}
+
+function wpAppbox_itunes_button() {
+	return "[appbox itunes "+tinyMCE.activeEditor.selection.getContent()+"]";
+}
+
+function wpAppbox_operaaddons_button() {
+	return "[appbox operaaddons "+tinyMCE.activeEditor.selection.getContent()+"]";
+}
+
+function wpAppbox_pebble_button() {
+	return "[appbox pebble "+tinyMCE.activeEditor.selection.getContent()+"]";
+}
+
+function wpAppbox_steam_button() {
+	return "[appbox steam "+tinyMCE.activeEditor.selection.getContent()+"]";
+}
+
+function wpAppbox_windowsstore_button() {
+	return "[appbox windowsstore "+tinyMCE.activeEditor.selection.getContent()+"]";
+}
+
+function wpAppbox_wordpress_button() {
+	return "[appbox wordpress "+tinyMCE.activeEditor.selection.getContent()+"]";
+}
