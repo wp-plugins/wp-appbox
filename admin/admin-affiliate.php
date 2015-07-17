5f4189afd8c41f19ee2fefe1b197847850d3d03c
Marcelismus <Marcelismus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-17 00:37:47 +0000

    Version 3.1.3
    git-svn-id: https://plugins.svn.wordpress.org/wp-appbox/tags/3.1.3@1200642 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/admin-affiliate.php
@@ -0,0 +1,47 @@
+<h3><?php _e('(Mac) App Store &amp; App Store: PHG', 'wp-appbox'); ?></h3>
+<p><?php _e('Have an affiliate ID at PHG? If so, enter it here and your app store links will be provided with your affiliate ID attached. No PHG account?', 'wp-appbox'); ?> <a href="http://affiliate.itunes.apple.com/de/apply" target="_blank"><?php _e('Here you can get one.', 'wp-appbox'); ?></a></p>
+
+<table class="form-table">
+	<tr valign="top">
+		<th scope="row"><label for="wpAppbox_affid"><?php _e('Affiliate Token', 'wp-appbox'); ?>:</label></th>
+		<td>
+			<input type="text" name="wpAppbox_affid" id="wpAppbox_affid" value="<?php echo $options['wpAppbox_affid']; ?>" />
+		</td>
+	</tr>
+	<tr valign="top">
+		<th scope="row"><label for="wpAppbox_affid_sponsored"><?php _e('Use the ID of the developer', 'wp-appbox'); ?>:</label></th>
+		<td>	
+			<input type="checkbox" name="wpAppbox_affid_sponsored" id="wpAppbox_affid_sponsored" value="1" <?php checked($options['wpAppbox_affid_sponsored']); ?>/> <?php _e('I have no ID at Apple/PHG and would like to use the developer-ID. (In this case: <strong>Thanks!</strong>)', 'wp-appbox'); ?>
+		</td>
+	</tr>
+</table>
+
+<h3><?php _e('Amazon Apps: Amazon PartnerNet', 'wp-appbox'); ?></h3>
+<p><?php _e('Have an affiliate ID at Amazon PartnerNet? If so, enter it here and your app store links will be provided with your affiliate ID attached. No Amazon PartnerNet account?', 'wp-appbox'); ?> <a href="https://partnernet.amazon.de/" target="_blank"><?php _e('Here you can get one.', 'wp-appbox'); ?></a></p>
+
+<table class="form-table">
+	<tr valign="top">
+		<th scope="row"><label for="wpAppbox_affid_amazonpartnernet"><?php _e('Amazon PartnerNet ID', 'wp-appbox'); ?>:</label></th>
+		<td>	
+			<input type="text" name="wpAppbox_affid_amazonpartnernet" id="wpAppbox_affid_amazonpartnernet" value="<?php echo $options['wpAppbox_affid_amazonpartnernet']; ?>" />
+		</td>
+	</tr>
+	<tr valign="top">
+		<th scope="row"><label for="wpAppbox_affid_amazonpartnernet_sponsored"><?php _e('Use the ID of the developer', 'wp-appbox'); ?>:</label></th>
+		<td>	
+			<input type="checkbox" name="wpAppbox_affid_amazonpartnernet_sponsored" id="wpAppbox_affid_amazonpartnernet_sponsored" value="1" <?php checked($options['wpAppbox_affid_amazonpartnernet_sponsored']); ?>/> <?php _e('I have no affiliate ID at Amazon PartnerNet and want to use the ID of the developer. (In this case: <strong>Thank you</strong>!)', 'wp-appbox'); ?>
+		</td>
+	</tr>
+</table>
+
+<h3><?php _e('Custon affiliate IDs', 'wp-appbox'); ?></h3>
+<p><?php _e('Each user and author can use his own affiliate IDs provided under his name an article is published. If no ID is specified in the user profile, the global IDs are used.', 'wp-appbox'); ?></p>
+
+<table class="form-table">
+	<tr valign="top">
+		<th scope="row"><label for="wpAppbox_user_affiliateids"><?php _e('Activate custom ID', 'wp-appbox'); ?>:</label></th>
+		<td>	
+			<input type="checkbox" name="wpAppbox_user_affiliateids" id="wpAppbox_user_affiliateids" value="1" <?php checked($options['wpAppbox_user_affiliateids']); ?><?php _e('Activates the possibility to use custom affiliate IDs for each user', 'wp-appbox'); ?>
+		</td>
+	</tr>
+</table>
---
+++ tpl/simple.php
@@ -0,0 +1,17 @@
+<div class="wpappbox wpappbox-{APPIDHASH} simple {STORECSS}">
+	{QRCODE}
+	<div class="appicon">
+		<a href="{APPLINK}" title="{TITLE}"><img src="{ICON}" />{WATCHICON}</a>
+	</div>
+	<div class="applinks">
+		<div class="appbuttons">
+			<a href="{APPLINK}" title="{TITLE}">Download</a>
+			<span onMouseOver="jQuery('.wpappbox-{APPIDHASH} .qrcode').show();" onMouseOut="jQuery('.wpappbox-{APPIDHASH} .qrcode').hide();">QR-Code</span>
+		</div>
+	</div>
+	<div class="appdetails">
+		<div class="apptitle">{RELOADLINK}<a href="{APPLINK}" title="{TITLE}" class="apptitle">{TITLE}</a></div>
+		<div class="developer"><?php _e('Developer', 'wp-appbox'); ?>: {DEVELOPERLINK}</div>
+		<div class="price"><?php _e('Price', 'wp-appbox'); ?>: {PRICE} {RATING}</div>
+	</div>
+</div>
